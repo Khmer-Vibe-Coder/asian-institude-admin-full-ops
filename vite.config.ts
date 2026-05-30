58ac215ad900b51cd059d9ec063e80d01cfe1521
CheaIsing <cheaising07@gmail.com>
2026-05-30 21:43:42 +0700

    fix err

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,12 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   // ❌ DO NOT include tanstackStart.server at all
+  nitro: {
+    preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+      serverDir: ".vercel/output/functions/__server.func",
+      publicDir: ".vercel/output/static",
+    },
+  },
 });
